13c4de12fad0197c954d9df4406616984f160c3a
Peter Ryan <147430628+peterryan12@users.noreply.github.com>
2025-02-24 23:53:33 -0600

    Add spinner

--- src/components/FilterHeading.jsx
+++ src/components/FilterHeading.jsx
@@ -13,7 +13,10 @@ export const FilterHeading = (props) => {
     }
 
     return <div className="flex justify-between mt-5">
-        <h1 className="text-3xl">{userCity}</h1>
+        <div className="flex gap-3 items-center ">
+        <h1 className="text-3xl">{userCity}</h1>      
+  <div class="w-8 h-8 border-4 border-t-4 border-blue-500 border-solid rounded-full animate-spin" id="spinner"></div>
+  </div>
         <div className="flex items-center gap-1 relative">
         <div>{props.roomCount ? props.roomCount : 1} rooms</div>
         <button onClick={props.toggleDropdown}> <Menu size={20} /></button>
